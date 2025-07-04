69c35500c9e4e14fcd8de0fb81e840d85fd5d319
Park Hyundo <hyundo13@naver.com>
2025-07-04 10:17:21 +0900

    feat: initial project

---
+++ src/test/java/com/grepp/teamnotfound/Web56404TnfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepp.teamnotfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web56404TnfBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
