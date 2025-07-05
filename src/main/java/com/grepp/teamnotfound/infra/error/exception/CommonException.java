9780ba11176d682418123eea3d66cb1ec4fbced3
ash373758 <ash373758@gmail.com>
2025-07-05 23:39:42 +0900

    Feat : ErrorCode 추가 및 도메인별 enum 분류

--- src/main/java/com/grepp/teamnotfound/infra/error/exception/CommonException.java
+++ src/main/java/com/grepp/teamnotfound/infra/error/exception/CommonException.java
@@ -1,13 +1,14 @@
 package com.grepp.teamnotfound.infra.error.exception;
 
+import com.grepp.teamnotfound.infra.error.exception.code.BaseErrorCode;
 import lombok.Getter;
 
 @Getter
 public class CommonException extends RuntimeException {
 
-    private final ErrorCode errorCode;
+    private final BaseErrorCode errorCode;
 
-    public CommonException(ErrorCode errorCode) {
+    public CommonException(BaseErrorCode errorCode) {
         this.errorCode = errorCode;
     }
 
