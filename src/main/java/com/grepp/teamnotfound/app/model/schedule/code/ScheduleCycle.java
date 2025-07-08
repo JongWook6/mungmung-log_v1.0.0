a606e1eecc5ea3826227d7a499c67ee4fa9e164b
kdg010729 <kdg010729@naver.com>
2025-07-08 11:33:01 +0900

    feat : ScheduleCycle에 날짜구하는 메서드 구현

--- src/main/java/com/grepp/teamnotfound/app/model/schedule/code/ScheduleCycle.java
+++ src/main/java/com/grepp/teamnotfound/app/model/schedule/code/ScheduleCycle.java
@@ -1,5 +1,19 @@
 package com.grepp.teamnotfound.app.model.schedule.code;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public enum ScheduleCycle {
-    WEEK, ONE_MONTH, THREE_MONTH, SIX_MONTH, YEAR
+    WEEK, ONE_MONTH, THREE_MONTH, SIX_MONTH, YEAR;
+
+    public int getDays(LocalDate date){
+        return switch (this) {
+            case WEEK -> 7;
+            case ONE_MONTH -> Period.between(date, date.plusMonths(1)).getDays();
+            case THREE_MONTH -> Period.between(date, date.plusMonths(3)).getDays();
+            case SIX_MONTH -> Period.between(date, date.plusMonths(6)).getDays();
+            case YEAR -> Period.between(date, date.plusYears(1)).getDays();
+            default -> 0;
+        };
+    }
 }
