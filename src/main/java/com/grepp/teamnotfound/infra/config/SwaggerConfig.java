a586c8fd6f98b51836817ab66891153df145b63b
kdg010729 <kdg010729@naver.com>
2025-07-09 17:52:04 +0900

    fix : cors 해결3

--- src/main/java/com/grepp/teamnotfound/infra/config/SwaggerConfig.java
+++ src/main/java/com/grepp/teamnotfound/infra/config/SwaggerConfig.java
@@ -5,12 +5,15 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
+    String jwt = "JWT";
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+    
     @Bean
     public OpenAPI openApiSpec() {
         return new OpenAPI()
@@ -33,5 +36,28 @@ public class SwaggerConfig {
                                                         "JWT 토큰을 입력하세요. Bearer 는 생략하세요")
                                 ))
                 .addSecurityItem(new SecurityRequirement().addList("404TNF"));
+
+        Server server = new Server();
+        server.setUrl("https://mungnote-172598302113.asia-northeast3.run.app");
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("API 문서")
+                        .description("API 명세입니다. 에러 코드는 [링크]를 참조해 주세요.")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth"
+                                , new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(
+                                                SecurityScheme.Type.HTTP)
+                                        .scheme(
+                                                "bearer")
+                                        .bearerFormat(
+                                                "JWT")
+                                        .description(
+                                                "JWT 토큰을 입력하세요. Bearer 는 생략하세요")))
+                .addServersItem(server);
     }
 }
