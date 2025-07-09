c7f0888f99955b2dce16a54d73c8b6e26aaecac3
kdg010729 <kdg010729@naver.com>
2025-07-09 17:27:54 +0900

    fix : cors 에러

---
+++ src/main/java/com/grepp/teamnotfound/infra/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.grepp.teamnotfound.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") // 모든 API 경로
+                .allowedOrigins("https://mungnote-172598302113.asia-northeast3.run.app") // 또는 "https://your-frontend.com"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
