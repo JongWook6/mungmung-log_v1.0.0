ae62493033ea5a328a5adcdc7eca26e710884e52
JongWook6 <jwpark4132@gmail.com>
2025-07-12 09:59:44 +0900

    feat : 게시판 관련 예외 추가

--- src/main/java/com/grepp/teamnotfound/app/model/board/ArticleService.java
+++ src/main/java/com/grepp/teamnotfound/app/model/board/ArticleService.java
@@ -1,22 +1,40 @@
 package com.grepp.teamnotfound.app.model.board;
 
+import com.grepp.teamnotfound.app.controller.api.article.payload.ArticleRequest;
 import com.grepp.teamnotfound.app.model.board.code.BoardType;
 import com.grepp.teamnotfound.app.model.board.code.SearchType;
 import com.grepp.teamnotfound.app.model.board.dto.ArticleListDto;
+import com.grepp.teamnotfound.app.model.board.entity.Article;
+import com.grepp.teamnotfound.app.model.board.entity.Board;
 import com.grepp.teamnotfound.app.model.board.repository.ArticleRepository;
+import com.grepp.teamnotfound.app.model.board.repository.BoardRepository;
+import com.grepp.teamnotfound.app.model.user.entity.User;
+import com.grepp.teamnotfound.app.model.user.repository.UserRepository;
+import com.grepp.teamnotfound.infra.error.exception.AuthException;
+import com.grepp.teamnotfound.infra.error.exception.BoardException;
+import com.grepp.teamnotfound.infra.error.exception.code.AuthErrorCode;
+import com.grepp.teamnotfound.infra.error.exception.code.BoardErrorCode;
+import com.grepp.teamnotfound.infra.error.exception.code.UserErrorCode;
+import com.grepp.teamnotfound.infra.util.file.GoogleStorageManager;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
     private final ModelMapper modelMapper;
+    private final GoogleStorageManager fileManager;
 
     public Page<ArticleListDto> findPaged(PageRequest pageable) {
 //        List articleRepository.findPaged(pageable);
@@ -33,5 +51,25 @@ public class ArticleService {
 
 
         return null;
+    }
+
+    public void save(ArticleRequest request, List<MultipartFile> images, String userEmail) {
+        // NOTE 로그인 필터를 거친 사용자인데 여기서도 검증이 플요한가? YES. 토큰 발급 시점과 현재 DB 사이의 무결성 검증
+        User user = userRepository.findByEmail(userEmail)
+            .orElseThrow(() -> new AuthException(UserErrorCode.USER_NOT_FOUND));
+
+        Board board = boardRepository.findByName(request.getBoardType().name())
+            .orElseThrow(() -> new BoardException(BoardErrorCode.BOARD_NOT_FOUND));
+
+        Article.builder()
+            .title(request.getTitle())
+            .content(request.getContent())
+            .user(user)
+            .board(board)
+            .build();
+
+        // TODO upload 실패시 IOException 해결
+
+
     }
 }
