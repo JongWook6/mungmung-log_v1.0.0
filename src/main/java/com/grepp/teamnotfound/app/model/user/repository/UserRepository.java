f307db6d29b671aece718bd3b54f86ff35354e1a
ash373758 <ash373758@gmail.com>
2025-07-14 11:56:12 +0900

    Refactor : User-UserImg 엔티티 단방향 설계로 분리 및 관련 로직 수정, findByUserId Repo 메서드 Optional 반환 처리

--- src/main/java/com/grepp/teamnotfound/app/model/user/repository/UserRepository.java
+++ src/main/java/com/grepp/teamnotfound/app/model/user/repository/UserRepository.java
@@ -12,5 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
 
     //TODO Optional<User>
-    User findByUserId(Long userId);
+    Optional<User> findByUserId(Long userId);
 }
