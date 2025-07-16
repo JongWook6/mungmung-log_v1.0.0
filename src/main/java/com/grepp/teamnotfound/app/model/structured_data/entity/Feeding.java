0d01b1da18eb443eab2b954b88dcbcff9b0f030f
Park Hyundo <hyundo13@naver.com>
2025-07-16 10:53:04 +0900

    refactor: Feeding, Walking 테이블 변경

--- src/main/java/com/grepp/teamnotfound/app/model/structured_data/entity/Feeding.java
+++ src/main/java/com/grepp/teamnotfound/app/model/structured_data/entity/Feeding.java
@@ -1,6 +1,6 @@
 package com.grepp.teamnotfound.app.model.structured_data.entity;
 
-import com.grepp.teamnotfound.app.model.pet.entity.Pet;
+import com.grepp.teamnotfound.app.model.life_record.entity.LifeRecord;
 import com.grepp.teamnotfound.app.model.structured_data.FeedUnit;
 import com.grepp.teamnotfound.infra.entity.BaseEntity;
 import jakarta.persistence.Column;
@@ -15,7 +15,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -51,11 +50,8 @@ public class Feeding extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private FeedUnit unit;
 
-    @Column(nullable = false)
-    private LocalDate recordedAt;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "pet_id", nullable = false)
-    private Pet pet;
+    @JoinColumn(name = "life_record_id", nullable = false)
+    private LifeRecord lifeRecord;
 
 }
