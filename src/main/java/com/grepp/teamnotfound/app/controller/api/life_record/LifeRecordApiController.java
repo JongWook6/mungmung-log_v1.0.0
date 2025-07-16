58d0f6d3a5ff424b3b73c40153786900fe822e49
Park Hyundo <hyundo13@naver.com>
2025-07-16 17:03:14 +0900

    refactor: 경로 수정 및, @Operation 추가

--- src/main/java/com/grepp/teamnotfound/app/controller/api/life_record/LifeRecordApiController.java
+++ src/main/java/com/grepp/teamnotfound/app/controller/api/life_record/LifeRecordApiController.java
@@ -6,6 +6,7 @@ import com.grepp.teamnotfound.app.model.life_record.LifeRecordService;
 import com.grepp.teamnotfound.app.model.life_record.dto.LifeRecordDto;
 import com.grepp.teamnotfound.app.model.note.NoteService;
 import com.grepp.teamnotfound.app.model.pet.PetService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -17,9 +18,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,7 +35,7 @@ public class LifeRecordApiController {
     private final LifeRecordService lifeRecordService;
     private final NoteService noteService;
 
-    // 보호자의 반려견 생활기록 리스트 조회
+    @Operation(summary = "생활기록 리스트 조회")
     @GetMapping("/v1/users/{userId}")
     public ResponseEntity<Map<String, List<LifeRecordListResponse>>> getLifeRecordList(
             @PathVariable Long userId,
@@ -63,7 +64,7 @@ public class LifeRecordApiController {
         return ResponseEntity.ok(Map.of("data", list));
     }
 
-    // 보호자의 반려견 목록 조회 (특정 반려견 생활기록만 보기 위하여 필요)
+    @Operation(summary = "보호자의 반려견 목록 조회") // 특정 반려견 생활기록만 보기 위하여 필요
     @GetMapping("/v1/users/{userId}/pet-list")
     public ResponseEntity<Map<String, List<Map<Long, String>>>> getPetList(
             @PathVariable Long userId
@@ -83,18 +84,17 @@ public class LifeRecordApiController {
         return ResponseEntity.ok(Map.of("data",list));
     }
 
-    // 생활기록 상세정보 조회
+    @Operation(summary = "생활기록 상세정보 조회")
     @GetMapping("/v2/detail/{lifeRecordId}")
     public ResponseEntity<Map<String, LifeRecordData>> getLifeRecordDetail(
-        @PathVariable Long lifeRecordId
+            @PathVariable Long lifeRecordId
     ){
         LifeRecordData lifeRecord = lifeRecordService.getLifeRecord(lifeRecordId);
 
         return ResponseEntity.ok(Map.of("data", lifeRecord));
     }
 
-
-    // 날짜, 애완동물 기준으로 기존 생활기록 데이터 있는지 체크
+    @Operation(summary = "기존 생활기록 데이터 존재 여부 체크")
     @GetMapping("/v2/pets/{petId}/check")
     public ResponseEntity<?> checkLifeRecord(
             @PathVariable Long petId,
@@ -113,8 +113,8 @@ public class LifeRecordApiController {
         return ResponseEntity.ok("데이터 등록 가능");
     }
 
-    // 생활기록 등록
-    @PostMapping
+    @Operation(summary = "생활기록 등록")
+    @PostMapping("/v2/create")
     public ResponseEntity<String> registLifeRecord(
             @RequestBody LifeRecordData data
     ){
@@ -124,8 +124,8 @@ public class LifeRecordApiController {
         return ResponseEntity.ok("등록 성공");
     }
 
-    // 생활기록 수정
-    @PutMapping("/v2/{lifeRecordId}/update")
+    @Operation(summary = "생활기록 수정")
+    @PatchMapping("/v2/{lifeRecordId}/update")
     public ResponseEntity<String> modifyLifeRecord(
             @PathVariable Long lifeRecordId,
             @RequestBody LifeRecordData data
@@ -136,7 +136,7 @@ public class LifeRecordApiController {
         return ResponseEntity.ok("수정 성공");
     }
 
-    // 생활기록 삭제
+    @Operation(summary = "생활기록 삭제")
     @DeleteMapping("/v2/{lifeRecordId}/delete")
     public ResponseEntity<String> deleteLifeRecord(
             @PathVariable Long lifeRecordId
