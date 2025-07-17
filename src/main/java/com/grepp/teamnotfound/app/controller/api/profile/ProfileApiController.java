ca2f327e6d2a051211296f931b26c87d60b0a0a5
eonwy <yeawon6566@gmail.com>
2025-07-17 15:05:51 +0900

    fix : 특정 회원 프로필 조회는 userId를 넣어주면 조회가 되게끔 수정

--- src/main/java/com/grepp/teamnotfound/app/controller/api/profile/ProfileApiController.java
+++ src/main/java/com/grepp/teamnotfound/app/controller/api/profile/ProfileApiController.java
@@ -1,7 +1,6 @@
 package com.grepp.teamnotfound.app.controller.api.profile;
 
 import com.grepp.teamnotfound.app.controller.api.profile.payload.ProfilePetResponse;
-import com.grepp.teamnotfound.app.model.auth.domain.Principal;
 import com.grepp.teamnotfound.app.model.pet.PetService;
 import com.grepp.teamnotfound.app.model.user.UserService;
 import com.grepp.teamnotfound.app.model.user.dto.UserDto;
@@ -9,9 +8,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,26 +22,17 @@ public class ProfileApiController {
     private final UserService userService;
     private final PetService petService;
 
-    @GetMapping("/v1")
-    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/v1/users/{userId}")
     public ResponseEntity<UserDto> getUser(
-        @AuthenticationPrincipal Principal principal
+        @PathVariable Long userId
     ) {
-        Long userId = null;
-        if (principal != null) {
-            userId = principal.getUserId();
-        }
-
         return ResponseEntity.ok(userService.findByUserId(userId));
     }
 
-    @GetMapping("/v1/pet")
-    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/v1/users/{userId}/pet")
     public ResponseEntity<List<ProfilePetResponse>> getUserPets(
-        @AuthenticationPrincipal Principal principal
+        @PathVariable Long userId
     ) {
-        Long userId = principal.getUserId();
-
         List<ProfilePetResponse> response = petService.findByUserId(userId);
         return ResponseEntity.ok(response);
     }
