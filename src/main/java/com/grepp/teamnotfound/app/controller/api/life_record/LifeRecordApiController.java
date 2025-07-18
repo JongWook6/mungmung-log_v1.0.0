2c58604bc35a8b181678fed444ba5bf1c68d8435
Park Hyundo <hyundo13@naver.com>
2025-07-18 11:40:34 +0900

    feat: 생활기록 리스트 출력 (필터링 포함)

--- src/main/java/com/grepp/teamnotfound/app/controller/api/life_record/LifeRecordApiController.java
+++ src/main/java/com/grepp/teamnotfound/app/controller/api/life_record/LifeRecordApiController.java
@@ -1,25 +1,29 @@
 package com.grepp.teamnotfound.app.controller.api.life_record;
 
+import com.grepp.teamnotfound.app.controller.api.article.payload.PageInfo;
+import com.grepp.teamnotfound.app.controller.api.life_record.payload.LifeRecordListRequest;
 import com.grepp.teamnotfound.app.controller.api.life_record.payload.LifeRecordListResponse;
 import com.grepp.teamnotfound.app.controller.api.life_record.payload.LifeRecordData;
 import com.grepp.teamnotfound.app.model.auth.domain.Principal;
 import com.grepp.teamnotfound.app.model.life_record.LifeRecordService;
 import com.grepp.teamnotfound.app.model.life_record.dto.LifeRecordDto;
+import com.grepp.teamnotfound.app.model.life_record.dto.LifeRecordListDto;
 import com.grepp.teamnotfound.app.model.pet.PetService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,32 +42,25 @@ public class LifeRecordApiController {
 
     @Operation(summary = "생활기록 리스트 조회")
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/v1/users/{userId}")
-    public ResponseEntity<Map<String, List<LifeRecordListResponse>>> getLifeRecordList(
-            @RequestParam(required = false) Long petId,
-            @RequestParam Integer page,
-            @RequestParam Integer size
+    @GetMapping("/v1/users/life-record-list")
+    public ResponseEntity<LifeRecordListResponse> getLifeRecordList(
             @AuthenticationPrincipal Principal principal,
+            @ModelAttribute @Valid LifeRecordListRequest request
     ){
-        // TODO: 생활기록 리스트 조회 Service 구현
+        PageRequest pageable = PageRequest.of(request.getPage() - 1, 12); // size 12 고정
 
-        // Mock Data
-        LifeRecordListResponse record1 = LifeRecordListResponse.builder()
-                .createdAt(OffsetDateTime.now())
-                .noteId(1L).name("이마음").savePath("이미지 저장경로").weight(36.3F)
-                .content("더워서 기분 안 좋아 보임").build();
-        LifeRecordListResponse record2 = LifeRecordListResponse.builder()
-                .createdAt(OffsetDateTime.now())
-                .noteId(1L).name("이마음").savePath("이미지 저장경로").weight(36.3F)
-                .content("더워서 기분 안 좋아 보임").build();
-        LifeRecordListResponse record3 = LifeRecordListResponse.builder()
-                .createdAt(OffsetDateTime.now())
-                .noteId(1L).name("이마음").savePath("이미지 저장경로").weight(36.3F)
-                .content("더워서 기분 안 좋아 보임").build();
+        Page<LifeRecordListDto> page = lifeRecordService.searchLifeRecords(principal.getUserId(), request, pageable);
 
-        List<LifeRecordListResponse> list = new ArrayList<>(List.of(record1, record2, record3));
+        PageInfo pageInfo = PageInfo.builder()
+                .page(page.getNumber() + 1)
+                .size(page.getSize())
+                .totalPages(page.getTotalPages())
+                .totalElements((int) page.getTotalElements())
+                .hasNext(page.hasNext())
+                .hasPrevious(page.hasPrevious())
+                .build();
 
-        return ResponseEntity.ok(Map.of("data", list));
+        return ResponseEntity.ok(new LifeRecordListResponse(page.getContent(), pageInfo));
     }
 
     @Operation(summary = "보호자의 반려견 목록 조회") // 특정 반려견 생활기록만 보기 위하여 필요
