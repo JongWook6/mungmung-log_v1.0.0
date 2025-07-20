e09bc399ee5180df27dd857cc640f398dad5cedf
Park Hyundo <hyundo13@naver.com>
2025-07-21 01:56:49 +0900

    fix: 생활기록 리스트에서 반려견 이미지가 없을 경우 발생하는 오류 수정

--- src/main/java/com/grepp/teamnotfound/app/model/life_record/dto/LifeRecordListPetDto.java
+++ src/main/java/com/grepp/teamnotfound/app/model/life_record/dto/LifeRecordListPetDto.java
@@ -1,5 +1,6 @@
 package com.grepp.teamnotfound.app.model.life_record.dto;
 
+import com.grepp.teamnotfound.app.model.pet.entity.Pet;
 import com.grepp.teamnotfound.app.model.pet.entity.PetImg;
 import com.grepp.teamnotfound.infra.code.ImgType;
 import lombok.AllArgsConstructor;
@@ -15,14 +16,15 @@ public class LifeRecordListPetDto {
     private String url;
     private ImgType type;
 
-    public static LifeRecordListPetDto petInfoDto(PetImg petImg){
-        if (petImg == null) return null;
-
-        return new LifeRecordListPetDto(
-            petImg.getPet().getName(),
-            petImg.getSavePath() + petImg.getRenamedName(),
-            petImg.getType()
-        );
+    public static LifeRecordListPetDto petInfoDto(Pet pet) {
+        return pet.getImages().stream()
+                .findFirst()
+                .map(petImg -> new LifeRecordListPetDto(
+                        pet.getName(),
+                        petImg.getSavePath() + petImg.getRenamedName(),
+                        petImg.getType()
+                ))
+                .orElse(new LifeRecordListPetDto(pet.getName(), null, null));
     }
 
 }
