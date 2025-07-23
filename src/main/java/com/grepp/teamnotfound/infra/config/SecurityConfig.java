fc2f8157b1f8df351302448a4ee2b500c5e2d33a
ash373758 <ash373758@gmail.com>
2025-07-23 18:11:30 +0900

    Fix : 비회원일 시, 몇몇 get 요청 가능하도록 수정

--- src/main/java/com/grepp/teamnotfound/infra/config/SecurityConfig.java
+++ src/main/java/com/grepp/teamnotfound/infra/config/SecurityConfig.java
@@ -6,10 +6,6 @@ import com.grepp.teamnotfound.infra.auth.oauth2.OAuth2SuccessHandler;
 import com.grepp.teamnotfound.infra.auth.token.filter.AuthExceptionFilter;
 import com.grepp.teamnotfound.infra.auth.token.filter.JwtAuthenticationFilter;
 import com.grepp.teamnotfound.infra.auth.token.filter.LogoutFilter;
-import com.grepp.teamnotfound.infra.util.requestmatcher.RequestMatcherHolder;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -19,21 +15,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -43,18 +38,13 @@ public class SecurityConfig {
 
     private final AuthExceptionFilter authExceptionFilter;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final RequestMatcherHolder requestMatcherHolder;
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final ApplicationContext applicationContext;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        RequestMatcher permitAllMatcher = requestMatcherHolder.getRequestMatchersByMinRole(null);
-
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
@@ -64,15 +54,34 @@ public class SecurityConfig {
                                 .successHandler(applicationContext.getBean(OAuth2SuccessHandler.class))
                                 .failureHandler(applicationContext.getBean(OAuth2FailureHandler.class))
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers(permitAllMatcher).permitAll()
-                                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // 프리플라이트 허용
-                                .anyRequest().authenticated()
-                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-                .addFilterBefore(applicationContext.getBean(LogoutFilter.class), UsernamePasswordAuthenticationFilter.class)
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers("/", "/error", "/favicon.ico").permitAll()
+                        // 로그인/회원가입
+                        .requestMatchers(GET, "/api/auth/**").permitAll()
+                        .requestMatchers(POST, "/api/auth/**").permitAll()
+                        // 소셜로그인 화면 -> 프론트 연동 후 삭제 예정
+                        .requestMatchers("/social/login").permitAll()
+                        // 오류 페이지 -> 프론트 개발 실제 주소 연동 예정
+                        .requestMatchers("/error/**").permitAll()
+                        // swagger 관련
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                        // 프리플라이트 허용
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        // get 특정 게시판의 게시글 리스트 조회 /api/community/articles/v1
+                        // get 게시글 좋아요 개수 /api/community/articles/v1/{articleId}/like
+                        // get 게시글 상세 조회 /api/community/articles/v1/{articleId}
+                        // get 게시글 댓글 개수 /api/community/articles/v1/{articleId}/reply
+                        .requestMatchers(GET, "/api/community/articles/v1/**").permitAll()
+                        // get /api/profile/v1/users/{userId}
+                        // get /api/profile/v1/users/{userId}/pet
+                        // get /api/profile/v1/users/{userId}/board
+                        .requestMatchers(GET, "/api/profile/v1/users/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+        http.addFilterBefore(applicationContext.getBean(LogoutFilter.class), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(authExceptionFilter, JwtAuthenticationFilter.class);
 
@@ -90,6 +99,7 @@ public class SecurityConfig {
         corsConfig.setAllowedOriginPatterns(Arrays.asList(
                 // TODO 프론트 서버로 수정 필요
                 "http://localhost:3000",
+                "http://localhost:8080",
                 "https://mungdiary-172598302113.asia-northeast3.run.app"
         ));
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
