1a3bfe4aaf12773b1f96e57a9e70237521673d36
Park Hyundo <hyundo13@naver.com>
2025-07-23 16:55:56 +0900

    refactor: Recommend, DailyRecommend 합치기

--- src/main/java/com/grepp/teamnotfound/app/model/recommend/entity/Recommend.java
+++ src/main/java/com/grepp/teamnotfound/app/model/recommend/entity/Recommend.java
@@ -4,17 +4,22 @@ package com.grepp.teamnotfound.app.model.recommend.entity;
 import com.grepp.teamnotfound.app.model.pet.code.PetPhase;
 import com.grepp.teamnotfound.app.model.pet.code.PetSize;
 import com.grepp.teamnotfound.app.model.pet.code.PetType;
+import com.grepp.teamnotfound.app.model.pet.entity.Pet;
 import com.grepp.teamnotfound.app.model.recommend.code.RecommendState;
 import com.grepp.teamnotfound.infra.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,17 +44,8 @@ public class Recommend extends BaseEntity {
     )
     private Long recId;
 
-    @Column(nullable = false, length = 6)
-    @Enumerated(EnumType.STRING)
-    private PetPhase age;
-
-    @Column(nullable = false, length = 20)
-    @Enumerated(EnumType.STRING)
-    private PetType breed;
-
-    @Column(nullable = false, length = 20)
-    @Enumerated(EnumType.STRING)
-    private PetSize size;
+    @Column(nullable = false)
+    private LocalDate date;
 
     @Column(nullable = false, length = 20)
     @Enumerated(EnumType.STRING)
@@ -63,8 +59,11 @@ public class Recommend extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private RecommendState sleepingState;
 
-
-    @Column(nullable = false, length = 60)
+    @Column(nullable = false)
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
 
 }
