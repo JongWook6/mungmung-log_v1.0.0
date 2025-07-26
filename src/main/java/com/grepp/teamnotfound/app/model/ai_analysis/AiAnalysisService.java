5b34b0e625087c7cbff3ebb1834e9675752de9da
kdg010729 <kdg010729@naver.com>
2025-07-26 20:20:00 +0900

    feat : pet-profile 응답에 나이, 성별, 분석 추가

--- src/main/java/com/grepp/teamnotfound/app/model/ai_analysis/AiAnalysisService.java
+++ src/main/java/com/grepp/teamnotfound/app/model/ai_analysis/AiAnalysisService.java
@@ -3,9 +3,14 @@ package com.grepp.teamnotfound.app.model.ai_analysis;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 
 @Service
 public class AiAnalysisService {
 
+    public String getAiAnalysis(Long petId, LocalDate date) {
+        return null;
+    }
 }
 
