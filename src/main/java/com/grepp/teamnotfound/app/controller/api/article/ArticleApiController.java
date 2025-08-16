e63d609344aea0a2803ab91f48ac2fc263287fce
Park Jong-wook <jwpark4132@gmail.com>
2025-08-16 16:45:14 +0900

    fix: 좋아요 요청 캐시 복구

--- src/main/java/com/grepp/teamnotfound/app/controller/api/article/ArticleApiController.java
+++ src/main/java/com/grepp/teamnotfound/app/controller/api/article/ArticleApiController.java
@@ -104,8 +104,7 @@ public class ArticleApiController {
         @PathVariable Long articleId,
         @AuthenticationPrincipal Principal principal
     ) {
-//        LikeResponse response = articleService.likeWithRedis(articleId, principal.getUserId());
-        LikeResponse response = articleService.likeArticle(articleId, principal.getUserId());
+        LikeResponse response = articleService.likeWithRedis(articleId, principal.getUserId());
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
@@ -116,8 +115,7 @@ public class ArticleApiController {
         @PathVariable Long articleId,
         @AuthenticationPrincipal Principal principal
     ) {
-//        LikeResponse response = articleService.unlikeWithRedis(articleId, principal.getUserId());
-        LikeResponse response = articleService.unlikeArticle(articleId, principal.getUserId());
+        LikeResponse response = articleService.unlikeWithRedis(articleId, principal.getUserId());
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
